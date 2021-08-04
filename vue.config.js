affc08807b8c14886be93d62395069dc10afd170
yoyo <po8867@gmail.com>
2021-08-04 14:40:00 +0800

    fix: 🐛modify vue config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 require('dotenv').config({ path: './.env' })
-const { loadEnvironments } = require('./webpack-util')
 module.exports = function () {
-  const envFromDotFile = loadEnvironments('./.env')
-  console.log(envFromDotFile)
-  Object.assign(envFromDotFile, process.env)
-  Object.assign(process.env, envFromDotFile)
   return {
     transpileDependencies: [
       'vuetify'
